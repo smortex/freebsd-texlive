8ea332e0416db457f50f26b1c5b2c99eef6d3b33
Romain Tartière <romain@blogreen.org>
2008-12-27 19:44:54 +0000

    print/texlive: Generate ls-R files when installing. Fix installation of macros (COPYTREE_SHARE has side effects) and do not include bsd.port.mk.

--- print/texlive/bsd.texlive.mk
+++ print/texlive/bsd.texlive.mk
@@ -4,7 +4,10 @@ PORTVERSION?=		${TEXLIVE_RELEASE}
 MASTER_SITES?=		${MASTER_SITE_TEX_CTAN}
 MASTER_SITE_SUBDIR?=	systems/texlive/tlnet/${TEXLIVE_RELEASE}/archive
 DIST_SUBDIR=		TeXLive/${TEXLIVE_RELEASE}
+PKGNAMEPREFIX=		texlive-
 #DISTNAME=		${PORTNAME}
+
+RUN_DEPENDS+=		mktexlsr:${PORTSDIR}/print/texlive
 
 DISTFILES=		${PORTNAME}${EXTRACT_SUFX}
 .if !defined(NOPORTDOCS)
@@ -28,15 +31,15 @@ pre-install:
 		if [ -d texmf-dist ];then ${FIND} texmf-dist -type d | sort -r | sed -e 's|^|@dirrmtry share/|' ; fi ;\
 		if [ -d texmf ]; then ${FIND} texmf -type d | sort -r | sed -e 's|^|@dirrmtry share/|' ; fi ;\
 	) > ${PLIST}
+	@echo "Fixing permissions..."
+	${CHMOD} -R =rw,+X ${WRKDIR}
+	${CHOWN} -R root:wheel ${WRKDIR}
 
 do-install:
-	cd ${WRKDIR} && ( \
-		if [ -d texmf ]; then ${COPYTREE_SHARE} texmf ${PREFIX}/share/ || exit 1 ; fi ;\
-		if [ -d texmf-dist ]; then ${COPYTREE_SHARE} texmf-dist ${PREFIX}/share/ || exit 1 ; fi ;\
-	)
+	if [ -d ${WRKDIR}/texmf ]; then cp -a ${WRKDIR}/texmf ${PREFIX}/share; fi
+	if [ -d ${WRKDIR}/texmf-dist ]; then cp -a ${WRKDIR}/texmf-dist ${PREFIX}/share; fi
 
 post-install:
 	@echo "Updating ls-R databases..."
 	@mktexlsr
 
-.include <bsd.port.mk>
