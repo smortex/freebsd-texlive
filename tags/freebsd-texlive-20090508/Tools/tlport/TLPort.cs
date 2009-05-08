168255532dfcd826e95131f48eabac219c64469f
romain.tartiere <romain.tartiere@0e36b83c-d12f-11dd-b6b4-918b84bbd919>
2009-05-08 03:09:18 +0000

    TAG freebsd-texlive-20090508

---
+++ tags/freebsd-texlive-20090508/Tools/tlport/TLPort.cs
@@ -0,0 +1,76 @@
+// Copyright (c) 2008, Romain Tartière <romain@blogreen.org>
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following condition are met:
+//
+//    * Redistributions of source code must retain the above copyright notice,
+//      this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+using System;
+using System.Collections.Generic;
+
+// This program will build all ports for TeXLive except print/texlive-core
+// which is provided in the freebsd-texlive Subversion repository.
+//
+// Before running this tool, you have to install print/texlice-core (since some
+// TeXLive packages include files that are provided by texlive-core, this
+// utility checks the filesystem to determine if files will be installed when
+// installing the package are skipped because they are part of texlive-core.
+//
+// You will have to specify path information at the command line. The first
+// argument is the location of a directory containing all .tlpobj files
+// provided in TeXLive tarballs, the second one is the location of the ports
+// tree where TeXLive ports are to be built.
+
+namespace TeXLive
+{
+	class TLPort
+	{
+
+		public static int Main(string[] args)
+		{
+			if (args.Length != 2) {
+				Console.Error.WriteLine("usage: tlport <tlpobjdir> <portsdir>");
+				return 1;
+			}
+
+			PackageCollection packages = new PackageCollection();
+			packages.TlpObjDir = args[0];
+			packages.PortsDir  = args[1];
+
+			Console.Error.WriteLine("===> Building package list (this takes a while)");
+			// Read all scheme-*. They reference all collections, that in turn
+			// reference each package.
+			foreach (string s in System.IO.Directory.GetFiles(packages.TlpObjDir, "scheme-*")) {
+				System.IO.FileInfo fi = new System.IO.FileInfo(s);
+				string package_name = fi.Name.Split('.')[0];
+
+				packages[package_name] = new DataPackage(package_name, packages);
+			}
+
+			foreach (Package pkg in packages.Values) {
+				if (pkg.Eligible) {
+					pkg.CreatePort();
+				} else {
+					Console.Error.WriteLine("{0}: not eligible for building a port.", pkg.Name);
+				}
+			}
+			return 0;
+		}
+	}
+}
