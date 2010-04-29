3a64498f9e8220bd9cd88fbffeb353260266d162
Romain Tartière <romain@blogreen.org>
2010-04-29 08:24:43 +0000

    Merge the freebsd-texlive-2009 branch into trunk.

--- print/texlive-core/bsd.texlive.mk
+++ print/texlive-core/bsd.texlive.mk
@@ -1,8 +1,8 @@
 
-TEXLIVE_RELEASE=	2008
+TEXLIVE_RELEASE=	2009
 PORTVERSION?=		${TEXLIVE_RELEASE}
 MASTER_SITES?=		${MASTER_SITE_TEX_CTAN}
-MASTER_SITE_SUBDIR?=	systems/texlive/tlnet/${TEXLIVE_RELEASE}/archive
+MASTER_SITE_SUBDIR?=	systems/texlive/tlnet/archive
 DIST_SUBDIR=		TeXLive/${TEXLIVE_RELEASE}
 PKGNAMEPREFIX=		texlive-
 #DISTNAME=		${PORTNAME}
@@ -15,32 +15,81 @@ DISTFILES+=		${PORTNAME}.doc${EXTRACT_SUFX}
 .endif
 .if !defined(NOPORTSRC)
 DISTFILES+=		${PORTNAME}.source${EXTRACT_SUFX}
+PLIST_SUB+=		PORTSRC=""
+.else
+PLIST_SUB+=		PORTSRC="@comment"
 .endif
 
-USE_LZMA=	yes
+FETCH_ARGS=	-ApR	# Do NOT restart a previously interrupted transfer
+USE_XZ=		yes
 NO_BUILD=	yes
 
-PLIST=		${WRKDIR}/pkg-plist
+${WRKDIR}/.install_files: build
+	@(  cd ${WRKDIR} && cat tlpkg/tlpobj/${PORTNAME}.tlpobj | awk '\
+		$$0 ~ /^ / { \
+		    source=$$1; \
+		    target="share/" $$1; \
+		    if (index($$1, "RELOC/")) { \
+			sub("RELOC/", "", source); \
+			sub("RELOC/", "texmf-dist/", target); \
+		    } \
+		    if (system("[ -e ${PREFIX}/" target " ]") != 0) { \
+			print source "	" target; \
+		    } \
+		}' > ${WRKDIR}/.install_files; \
+	)
+.if !defined(NOPORTDOCS)
+	@(  cd ${WRKDIR} && cat tlpkg/tlpobj/${PORTNAME}.doc.tlpobj | awk '\
+		$$0 ~ /^ / { \
+		    source=$$1; \
+		    target="share/" $$1; \
+		    if (index($$1, "RELOC/")) { \
+			sub("RELOC/", "", source); \
+			sub("RELOC/", "texmf-dist/", target); \
+		    } \
+		    if (system("[ -e ${PREFIX}/" target " ]") != 0) { \
+			print source "	" target "	%%PORTDOCS%%"; \
+		    } \
+		}' >> ${WRKDIR}/.install_files; \
+	)
+.endif
+.if !defined(NOPORTSRC)
+	@(  cd ${WRKDIR} && cat tlpkg/tlpobj/${PORTNAME}.source.tlpobj | awk '\
+		$$0 ~ /^ / { \
+		    source=$$1; \
+		    target="share/" $$1; \
+		    if (index($$1, "RELOC/")) { \
+			sub("RELOC/", "", source); \
+			sub("RELOC/", "texmf-dist/", target); \
+		    } \
+		    if (system("[ -e ${PREFIX}/" target " ]") != 0) { \
+			print source "	" target "	%%PORTSRC%%"; \
+		    } \
+		}' >> ${WRKDIR}/.install_files; \
+	)
+.endif
 
-pre-install:
-	@${RM} -f ${PLIST}
-	@echo "Generating pkg-plist..."
-	@cd ${WRKDIR} && ( \
-		if [ -d texmf ]; then ${FIND} texmf -type f -exec '[' '!' '-e' "${PREFIX}/share/{}" ']' ';' -print ; fi ;\
-		if [ -d texmf-dist ]; then ${FIND} texmf-dist -type f -exec '[' '!' '-e' "${PREFIX}/share/{}" ']' ';' -print ; fi ;\
-	) | tee ${WRKDIR}/.install_files | sed -e 's|^|share/|' > ${PLIST}
-	@cd ${WRKDIR} && ( \
-		if [ -d texmf-dist ];then ${FIND} texmf-dist -type d | sort -r | sed -e 's|^|@dirrmtry share/|' ; fi ;\
-		if [ -d texmf ]; then ${FIND} texmf -type d | sort -r | sed -e 's|^|@dirrmtry share/|' ; fi ;\
-	) >> ${PLIST}
-	@echo "Fixing permissions..."
-	@${CHMOD} -R =rw,+X ${WRKDIR}
-	@${CHOWN} -R root:wheel ${WRKDIR}
+pkg-plist: ${WRKDIR}/.install_files
+	@sort -k 2 < ${WRKDIR}/.install_files | awk ' { print $$3 $$2 } ' > ${PLIST}
+	@for dir in `cut -f 2 < ${WRKDIR}/.install_files | xargs dirname | sort -r | uniq`; do \
+	    if [ ! -d "${PREFIX}/$$dir" ]; then \
+		echo @dirrmtry $$dir >> ${PLIST} ;\
+	    fi; \
+	done
+	@echo "@exec %D/bin/mktexlsr" >> ${PLIST}
+	@echo "@unexec %D/bin/mktexlsr" >> ${PLIST}
 
-do-install:
-	@cat ${WRKDIR}/.install_files | tar cf - -C ${WRKDIR} -T - | tar xf - -p -C ${PREFIX}/share
+do-install: ${WRKDIR}/.install_files
+	@for dir in `cut -f 2 < ${WRKDIR}/.install_files | xargs dirname | sort -r | uniq`; do \
+	    if [ ! -d "${PREFIX}/$$dir" ]; then \
+		${MKDIR} "${PREFIX}/$$dir" ;\
+	    fi; \
+	done
+	@(  cd ${WRKDIR} && while read source target junk; do \
+		${INSTALL_DATA} $$source ${PREFIX}/$$target; \
+	    done < ${WRKDIR}/.install_files \
+	)
 
 post-install:
 	@echo "Updating ls-R databases..."
-	@mktexlsr
-
+	@${PREFIX}/bin/mktexlsr
