d8c77e1ab290f2e9accd8a0c5ac5f2b386cf6a4e
romain.tartiere <romain.tartiere@0e36b83c-d12f-11dd-b6b4-918b84bbd919>
2012-02-07 13:01:13 +0000

    Escape filenames to cope with '(', ')' and ' ' character.
    Submitted by:	Shin-ya Murakami <murashin@gfd-dennou.org>

--- trunk/print/texlive-core/bsd.texlive.mk
+++ trunk/print/texlive-core/bsd.texlive.mk
@@ -29,67 +29,67 @@ NO_WRKSUBDIR=	yes
 MKTEXLSR=	${PREFIX}/bin/mktexlsr
 
 ${WRKDIR}/.install_files: build
-	@(  cd ${WRKDIR} && ${CAT} tlpkg/tlpobj/${PORTNAME}.tlpobj | ${AWK} '\
-		$$0 ~ /^ / { \
-		    source=$$1; \
-		    target="share/" $$1; \
-		    if (index($$1, "RELOC/")) { \
+	@(  cd ${WRKDIR} && ${CAT} tlpkg/tlpobj/${PORTNAME}.tlpobj | ${GREP} ^\  | ${AWK} '\
+		{ \
+		    source=substr($$0, 2, length($$0)); \
+		    target="share/" source; \
+		    if (index(source, "RELOC/")) { \
 			sub("RELOC/", "", source); \
 			sub("RELOC/", "texmf-dist/", target); \
 		    } \
-		    if (system("[ -e ${PREFIX}/" target " ]") != 0) { \
-			print source "	" target; \
+		    if (system("[ -e \"${PREFIX}/" target "\" ]") != 0) { \
+			print source "," target ","; \
 		    } \
 		}' > ${WRKDIR}/.install_files; \
 	)
 .if !defined(NOPORTDOCS)
-	@(  cd ${WRKDIR} && ${CAT} tlpkg/tlpobj/${PORTNAME}.doc.tlpobj | ${AWK} '\
-		$$0 ~ /^ / { \
-		    source=$$1; \
-		    target="share/" $$1; \
-		    if (index($$1, "RELOC/")) { \
+	@(  cd ${WRKDIR} && ${CAT} tlpkg/tlpobj/${PORTNAME}.doc.tlpobj | ${GREP} ^\  | ${AWK} '\
+		{ \
+		    source=substr($$0, 2, length($$0)); \
+		    target="share/" source; \
+		    if (index(source, "RELOC/")) { \
 			sub("RELOC/", "", source); \
 			sub("RELOC/", "texmf-dist/", target); \
 		    } \
-		    if (system("[ -e ${PREFIX}/" target " ]") != 0) { \
-			print source "	" target "	%%PORTDOCS%%"; \
+		    if (system("[ -e \"${PREFIX}/" target "\" ]") != 0) { \
+			print source "," target ",%%PORTDOCS%%"; \
 		    } \
 		}' >> ${WRKDIR}/.install_files; \
 	)
 .endif
 .if !defined(NOPORTSRC)
-	@(  cd ${WRKDIR} && ${CAT} tlpkg/tlpobj/${PORTNAME}.source.tlpobj | ${AWK} '\
-		$$0 ~ /^ / { \
-		    source=$$1; \
-		    target="share/" $$1; \
-		    if (index($$1, "RELOC/")) { \
+	@(  cd ${WRKDIR} && ${CAT} tlpkg/tlpobj/${PORTNAME}.source.tlpobj | ${GREP} ^\  | ${AWK} '\
+		{ \
+		    source=substr($$0, 2, length($$0)); \
+		    target="share/" source; \
+		    if (index(source, "RELOC/")) { \
 			sub("RELOC/", "", source); \
 			sub("RELOC/", "texmf-dist/", target); \
 		    } \
-		    if (system("[ -e ${PREFIX}/" target " ]") != 0) { \
-			print source "	" target "	%%PORTSRC%%"; \
+		    if (system("[ -e \"${PREFIX}/" target "\" ]") != 0) { \
+			print source "," target ",%%PORTSRC%%"; \
 		    } \
 		}' >> ${WRKDIR}/.install_files; \
 	)
 .endif
 
 pkg-plist: ${WRKDIR}/.install_files
-	@${SORT} -k 2 < ${WRKDIR}/.install_files | ${AWK} ' { print $$3 $$2 } ' > ${PLIST}
-	@for dir in `${CUT} -f 2 < ${WRKDIR}/.install_files | ${XARGS} dirname | ${SORT} -r | uniq`; do \
+	@${SORT} -t',' -k 2 < ${WRKDIR}/.install_files | ${AWK} -F',' ' { print $$3 $$2 } ' > ${PLIST}
+	@for dir in `${CUT} -d',' -f 2 < ${WRKDIR}/.install_files | ${XARGS} dirname | ${SORT} -r | uniq`; do \
 	    if [ ! -d "${PREFIX}/$$dir" ]; then \
 		${ECHO} @dirrmtry $$dir >> ${PLIST} ;\
 	    fi; \
 	done
-	@echo "@exec %D/bin/mktexlsr" >> ${PLIST}
-	@echo "@unexec %D/bin/mktexlsr" >> ${PLIST}
+	@${ECHO} "@exec %D/bin/mktexlsr" >> ${PLIST}
+	@${ECHO} "@unexec %D/bin/mktexlsr" >> ${PLIST}
 
 do-install: ${WRKDIR}/.install_files
-	@for dir in `${CUT} -f 2 < ${WRKDIR}/.install_files | ${XARGS} dirname | ${SORT} -r | uniq`; do \
+	@for dir in `${CUT} -d',' -f 2 < ${WRKDIR}/.install_files | ${XARGS} dirname | ${SORT} -r | uniq`; do \
 	    if [ ! -d "${PREFIX}/$$dir" ]; then \
 		${MKDIR} "${PREFIX}/$$dir" ;\
 	    fi; \
 	done
-	@(  cd ${WRKDIR} && while read source target junk; do \
+	@( cd ${WRKDIR} && IFS="," && while read source target junk; do \
 		${INSTALL_DATA} $$source ${PREFIX}/$$target; \
 	    done < ${WRKDIR}/.install_files \
 	)
